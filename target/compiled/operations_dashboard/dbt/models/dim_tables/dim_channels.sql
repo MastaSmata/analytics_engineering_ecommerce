a00fc0e4837054d3370ed13f4f3601b924c7b7a9
Michael Adeyanju <73649369+MastaSmata@users.noreply.github.com>
2026-08-10 22:10:02 +0100

    Delete target directory

--- target/compiled/operations_dashboard/dbt/models/dim_tables/dim_channels.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    to_hex(md5(cast(coalesce(cast(channel_name as string), '_dbt_utils_surrogate_key_null_') as string))) AS channel_key,
-
-    LOWER(TRIM(channel_name)) AS channel_name,
-    LOWER(TRIM(channel_category)) AS channel_category,
-    LOWER(TRIM(traffic_type)) AS traffic_type
-    
-
-FROM `project-004c9430-f8fa-4b9a-861`.`ecommerce_data`.`stg_channels`
